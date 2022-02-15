3d7af3beb14a4da284065bb30469ac562bf57a7f
rami <ramighammami@gmail.com>
2022-02-15 07:05:33 +0300

    Before Using A Reducer

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -22,6 +22,14 @@ const HomeScreen = ({ navigation }) => {
 				onPress={() => navigation.navigate("Counter")}
 				title="Go to Counter Demo"
 			/>
+			<Button
+				onPress={() => navigation.navigate("Color")}
+				title="Go to Color screen Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Square")}
+				title="Go to Sqaure screen Demo"
+			/>
 		</View>
 	);
 };
